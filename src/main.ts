99da7c4b131e7f3252c50fcb96e9b1fa9c15eddc
lzc <1046492069@qq.com>
2022-05-06 10:20:51 +0800

    玩一玩

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import { createPinia } from 'pinia';
+import './assets/css/style.less';
+//引入normalize.css和base.css
+import 'normalize.css';
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,48 +1,86 @@
 <template>
   <div id="home">
-    <h2>vue+vite+ts+esline+prettier+less+vue-router+pinia</h2>
-    <div>pinia:{{ homeInfo.name }}</div>
-    <HelloWorld ref="HelloWorldRef" @helloWorldClick="helloWorldClick"></HelloWorld>
+    <div
+      v-for="(item, index) in imageData"
+      :key="index"
+      class="cs"
+      :class="activeIndex == index ? 'active' : ''"
+      :style="{ backgroundImage: 'url(' + item.url + ')' }"
+      @click="onClick(index)"
+    >
+      <div class="title" :class="activeIndex == index ? 'activeTile' : ''">{{ item.title }}</div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import homePinia from '../store/home';
-import { post } from '../network/index';
-import HelloWorld from '../components/HelloWorld.vue';
-import { onMounted, ref } from 'vue';
-const HelloWorldRef = ref<InstanceType<typeof HelloWorld>>();
-onMounted(() => {
-  if (HelloWorldRef.value) {
-    let a = HelloWorldRef.value.name;
-    console.log(a);
+import { ref } from 'vue';
+let activeIndex = ref(0);
+const onClick = (index: number) => {
+  activeIndex.value = index;
+};
+const imageData = [
+  {
+    url: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    title: 'Explore The World'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    title: 'Wild Forest'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+    title: ' Sunny Beach'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+    title: 'City on Winter'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    title: 'Mountains - Clouds'
   }
-});
-const helloWorldClick = (data: any) => {
-  console.log(data, 999);
-};
-interface cs {
-  name: string;
-}
-const a = async () => {
-  let aa = await post<cs>({
-    url: '/chat',
-    data: {
-      name: 1
-    },
-    isLoading: false,
-    haveToken: false
-  });
-  console.log(aa.name);
-};
-a();
-let homeInfo = homePinia();
-homeInfo.getHomeName();
+];
 </script>
-<style scoped>
+<style scoped lang="less">
 #home {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%);
+  position: relative;
+  display: flex;
+  justify-content: center;
+  transform: translate(0, 10vh);
+  width: 90vw;
+  align-items: center;
+  margin: 0 auto;
+  overflow: hidden;
+  .cs {
+    position: relative;
+    flex: 0.5;
+    height: 80vh;
+    border-radius: 50px;
+    margin-right: 60px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: all 0.7s ease-in;
+    &:last-child {
+      margin-right: 0;
+    }
+    .title {
+      position: absolute;
+      bottom: 23px;
+      left: 23px;
+      font-weight: 700;
+      font-size: 1.7em;
+      color: #fff;
+      opacity: 0;
+      white-space: nowrap;
+      transition: all 0.5s ease-in;
+    }
+  }
+  .active {
+    flex: 5;
+  }
+}
+.activeTile {
+  opacity: 1 !important;
 }
 </style>
